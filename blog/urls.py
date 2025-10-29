db3052320ce18ed92a73b4c3d5fe67bc0333a152
Palak <palakrana0921@gmail.com>
2025-10-29 20:25:42 +0530

    Updated urls.py to include logout route and authentication setup

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/<str:username>/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-
 ]
+
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,14 +1,11 @@
-# blogproject/urls.py
+# BlogWebapp/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # 👈 includes blog URLs at root
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('blog.urls')),  # includes your app URLs
+    path('accounts/', include('django.contrib.auth.urls')),  # for login/logout/password reset
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
